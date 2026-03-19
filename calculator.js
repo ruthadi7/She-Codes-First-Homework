ef4e9c11a7d497ca70b7174eeafa8a3c7b61dda2
ruthadi7 <Gashawruth@gmail.com>
2026-03-19 08:50:14 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -8,12 +8,17 @@ function multiply(a, b) {
  return a * b;
 }
 function divide(a, b) {
+   if(b==0){
+        console.log("ERROR, You have entered an invalid input")
+    }
+    else{
  return a / b;
+    }
 }
 console.log(add(3,2));
 console.log(subtract(3,2));
 console.log(multiply(3,2));
-console.log(divide(3,2));
+console.log(divide(3,0));
 
  
     
